f590228e8d180da9efe7e440fc927ec7cda237fd
Humphrey <mugagambi@gmail.com>
2020-11-10 01:15:21 +0300

    use env variable

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-git clone https://mugagambi:bfea62f0fc7bd92dee80dc11b8d77f708da2dab5@github.com/syntaxltd/silgred-desktop silgred-desktop
+git clone https://mugagambi:"$GH_TOKEN"@github.com/syntaxltd/competenz-med
 cd silgred-desktop
 npm install
 npm run dist
